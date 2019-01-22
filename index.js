bc1ae0c58188f19726862a28e20cdf62ac5d807e
metechy <31516925+metechy@users.noreply.github.com>
2019-01-21 21:49:21 -0700

    Add files via upload
    first cut

--- index.js
+++ index.js
@@ -1,30 +1,49 @@
 // setup server
 // YOUR CODE
+var express = require('express');
+var app     = express();
 
 // setup directory used to serve static files
 // YOUR CODE
+app.use(express.static('public'));
+
+// start server
+// -----------------------
+app.listen(3001, function(){
+    console.log('Running on port 3001');
+});
+
+// allow Cross-Origin Resource Sharing (CORS)
+var cors = require('cors');
+app.use(cors());
 
 // setup data store
 // YOUR CODE
+var low     = require('lowdb');
+var fs      = require('lowdb/adapters/FileSync');
+var adapter = new fs('db.json');
+var db      = low(adapter);
+
 
 // required data store structure
 // YOUR CODE
-/*
-{ 
-    accounts:[
-        {name        : '',
-         email       : '',
-         balance     : 0,
-         password    : '',
-         transactions: []}
-    ] 
-}
-*/
+db.defaults({accounts:[] }).write();
+
 
 app.get('/account/create/:name/:email/:password', function (req, res) {
 
     // YOUR CODE
     // Create account route
+    var account = {
+        "name" : req.params.name,
+        "email"    : req.params.email,
+        "balance"   : 0,
+        "password" : req.params.password,
+        "transactions" : []
+    };
+    db.get('accounts').push(account).write();
+    console.log(db.get('accounts').value());   
+    res.send("success");
     // return success or failure string
 });
 
@@ -32,6 +51,22 @@ app.get('/account/login/:email/:password', function (req, res) {
 
     // YOUR CODE
     // Login user - confirm credentials
+    var useremail = req.params.email;
+    var userpwd = req.params.password;
+    var userfound = db.get('accounts').find({email:useremail}).value();
+    if (userfound == null){
+        res.send(null);  
+    }
+    else {
+        if (userfound.password == userpwd){
+        res.send(userfound);    
+        }
+        else {
+        res.send(null);    
+        }
+    }
+    
+
     // If success, return account object    
     // If fail, return null
 });
